6a84c8071782b54e89c7c979130d135c9f31b921
Deian Stefan <deian@cs.ucsd.edu>
2020-04-27 19:43:32 -0700

    fix

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-This extension strips URLDefense's tracking from GMail pages. It's not clear to
-me that the after-the-fact click tracking provides any meaningful security. It's certainly silly when URLs to trustworthy sites are redirected via URLDefense.
+This extension strips URLDefense's tracking from GMail pages. There are
+security benefits to after-the-fact click tracking, but I personally don't want
+reveal which links I click on and when or share awkward links when I forward
+emails.
+
+Obviously use this at your own risk.
--- unurldefense.js
+++ unurldefense.js
@@ -1,8 +1,14 @@
-for (const a of document.getElementsByTagName('a')) {
-  if (a.hostname === "urldefense.com" && a.dataset.saferedirecturl) {
-    const original_src = a.dataset.saferedirecturl;
-    const href = original_src.replace(/.*urldefense.com\/v3\/__(.*)__;!!.*/,'$1');
-    a.href = href;
-    a.innerText = href;
-  }
-} 
+function stripURLs() {
+  for (const a of document.getElementsByTagName('a')) {
+    if (a.hostname === "urldefense.com" && a.dataset.saferedirecturl) {
+      const href = new URL(a.dataset.saferedirecturl).href;
+      a.href = href;
+      a.innerText = href;
+      a.dataset.saferedirecturl = href;
+    }
+  } 
+}
+
+// Strip URLs on document end.
+// TODO: do it again when new <a>'s are added
+stripURLs();
