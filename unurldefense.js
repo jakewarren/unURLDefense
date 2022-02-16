3578f704e17b122df6dea3c16a4490c2f89eff9a
jakewarren <jakewarren@users.noreply.github.com>
2022-02-16 16:35:40 -0600

    Update unurldefense.js

--- unurldefense.js
+++ unurldefense.js
@@ -1,14 +1,42 @@
+const urldefenseRegex = /https:\/\/urldefense.com\/.*?\/__(.*?)__;.*?\$/;
+
+function unUrlDefense(url) {
+  return url.replace(urldefenseRegex, "$1");
+}
+
+// process all links and remove the urldefense wrapper
 function stripURLs() {
   for (const a of document.getElementsByTagName('a')) {
     if (a.hostname === "urldefense.com" && a.dataset.saferedirecturl) {
-      const href = new URL(a.dataset.saferedirecturl).href;
-      a.href = href;
-      a.innerText = href;
-      a.dataset.saferedirecturl = href;
+      const href = new URL(a.href).href;
+      a.href = unUrlDefense(href);
+      a.dataset.saferedirecturl = unUrlDefense(href);
+      a.innerText = unUrlDefense(a.innerText)
     }
   } 
 }
 
-// Strip URLs on document end.
-// TODO: do it again when new <a>'s are added
-stripURLs();
+var observeDOM = (function(){
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+      eventListenerSupported = window.addEventListener;
+
+  return function(obj, callback){
+      if( MutationObserver ){
+          var obs = new MutationObserver(function(mutations, observer){
+              if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+                  callback();
+          });
+          obs.observe( obj, { childList:true, subtree:true });
+      }
+      else if( eventListenerSupported ){
+          obj.addEventListener('DOMNodeInserted', callback, false);
+          obj.addEventListener('DOMNodeRemoved', callback, false);
+      }
+  }
+})();
+
+// use a DOM MutationObserver to monitor for changes (like loading an email) and strip the urldefense URLS from all links
+// NOTE: monitoring the body element could potentially have performance impacts, need to monitor
+observeDOM( document.querySelector('body') ,function(){ 
+  stripURLs();
+});
