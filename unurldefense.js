c04bb918b415b80fbc946869ccd219df6662c030
jakewarren <jakewarren@users.noreply.github.com>
2022-04-11 12:43:54 -0500

    update decoding with logic from cphyc/thunderbird_remove_safelinks

--- unurldefense.js
+++ unurldefense.js
@@ -1,7 +1,38 @@
-const urldefenseRegex = /https:\/\/urldefense.com\/.*?\/__(.*?)__;.*?\$/;
+// source: https://github.com/cphyc/thunderbird_remove_safelinks/blob/e48b3f3bf547797cfc9db60cbcd43003c8a0f545/src/decoders.js
+function unUrlDefense(a) {
+  let proofpoint_regex = new RegExp('https://urldefense(?:\.proofpoint)?\.com/(v[0-9])/([.:]?[-+a-zA-Z0-9%&/=;_!?*$]+)+');
+  var proofpoint = a.match(proofpoint_regex);
+    if (!proofpoint)
+        return a;
 
-function unUrlDefense(url) {
-  return url.replace(urldefenseRegex, "$1").replace("*","#");
+    var v = proofpoint[1];
+    var outurl = a;
+    if (v == 'v1') {
+        let v1_pattern = new RegExp('https://urldefense(?:\.proofpoint)?\.com/v1/url\\?u=([^&]*)&k=.*');
+        outurl = decodeURIComponent(a.match(v1_pattern)[1]);
+    } else if (v == 'v2') {
+        let v2_pattern = new RegExp('https://urldefense(?:\.proofpoint)?\.com/v2/url\\?u=([^&]*)&[dc]=.*');
+        let url = a.match(v2_pattern)[1].replace(/-/g, '%').replace(/_/g, '/');
+        outurl = decodeURIComponent(url);
+    } else if (v == 'v3') {
+        let v3_pattern = new RegExp('https://urldefense(?:\.proofpoint)?\.com/v3/__(.+)__;([^\!]*).*');
+        let v3_token_pattern = new RegExp('\\*(\\*.)?', 'g');
+        let length_codes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+        let url = a.match(v3_pattern);
+        let encbytes = atob(url[2].replace(/_/g, '/').replace(/-/g, '+'));
+        let encbytes_off = 0;
+
+        outurl = url[1].replace(v3_token_pattern, (chunk) => {
+            var len = 1;
+            if (chunk.length > 1)
+               len = length_codes.search(chunk[2]) + 2;
+            var out = encbytes.substring(encbytes_off, encbytes_off + len);
+            encbytes_off += len;
+            return out;
+        });
+    }
+
+    return outurl;
 }
 
 // process all links and remove the urldefense wrapper
